8b3e5341217e2fefd234b7275883bdac0fc0ed56
Joao Guedes <isudjoao@gmail.com>
2021-05-20 20:15:47 -0300

    Implementaçao do parser e mudanmça dos dados do user

--- aluno.py
+++ aluno.py
@@ -1,5 +1,5 @@
 # Identifique-se preenchendo as strings neste arquivo
-nome = "Nome Completo"
-matricula = "12/3456789"
-email = "alunx@email.com"
-usuario_github = "gituser"
+nome = "João Pedro José Santos da Silva Guedes"
+matricula = "17/0013910"
+email = "isudjoao@gmail.com"
+usuario_github = "sudjoao"
--- parser.py
+++ parser.py
@@ -3,13 +3,35 @@ import math
 from lark import Lark, InlineTransformer, Token
 
 
-# Implemente a gramática aqui! Você pode testar manualmente seu código executando
-# o arquivo calc.py e testá-lo utilizando o pytest.
 grammar = Lark(
     r"""
-start : /\d+/
-""",
-    parser="lalr",
+    ?start  : assign* comp?
+    ?assign: NAME "=" comp
+    ?comp  : expr "<" expr  -> lt
+        | expr "<=" expr -> le
+        | expr ">" expr  -> gt
+        | expr ">=" expr -> ge
+        | expr "!=" expr -> ne
+        | expr "==" expr -> eq
+        | expr
+    ?term  : term "*" pow   -> mul
+        | term "/" pow   -> div
+        | pow
+    ?expr  : expr "-" term  -> sub
+        | expr "+" term  -> add
+        | term
+    ?pow   : atom "^" pow   -> exp
+        | atom
+    ?atom  : NUMBER                        -> number
+        | NAME "(" expr ")"             -> function_transform
+        | NAME "(" expr ("," expr)* ")" -> function_transform
+        | NAME                          -> var
+        | "(" expr ")"
+    NAME   : /[-+]?\w+/
+    NUMBER : /-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/
+    %ignore /\s+/
+    %ignore /\#.*/
+    """
 )
 
 
@@ -20,3 +42,32 @@ class CalcTransformer(InlineTransformer):
         super().__init__()
         self.variables = {k: v for k, v in vars(math).items() if not k.startswith("_")}
         self.variables.update(max=max, min=min, abs=abs)
+
+    def number(self, token):
+        try:
+            return int(token)
+        except:
+            return float(token)
+
+    def assign(self, name, value):
+        self.env[name] = value
+        return self.env[name]
+
+
+    def var(self, token):
+        if token.startswith("-") and token[1:] in self.variables:
+            return -self.variables[token[1:]]
+        elif token in self.variables:
+            return self.variables[token]
+        else:
+            return self.env[token]
+
+    def function_transform(self, name, *args):
+        name = str(name)
+        fn = self.variables[name.split('-')[-1]]
+        if name[0] == '-':
+            return -fn(*args)
+        return fn(*args)
+
+    def start(self, *args):
+        return args[-1]
