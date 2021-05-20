e3537890b1d9626d41dea0fcd64044b20dbe5a78
Joao Guedes <isudjoao@gmail.com>
2021-05-20 20:28:23 -0300

    Passando em outros testes

--- parser.py
+++ parser.py
@@ -2,26 +2,23 @@ import re
 import math
 from lark import Lark, InlineTransformer, Token
 
-
-# Implemente a gramática aqui! Você pode testar manualmente seu código executando
-# o arquivo calc.py e testá-lo utilizando o pytest.
 grammar = Lark(
     r"""
     ?start  : assign* comp?
     ?assign: NAME "=" comp
-    ?comp  : expr "<" expr  -> lt
-        | expr "<=" expr -> le
-        | expr ">" expr  -> gt
+    ?comp  : expr ">" expr  -> gt
         | expr ">=" expr -> ge
+        | expr "<" expr  -> lt
+        | expr "<=" expr -> le
         | expr "!=" expr -> ne
         | expr "==" expr -> eq
         | expr
+    ?expr  : expr "+" term  -> add
+        | expr "-" term  -> sub
+        | term
     ?term  : term "*" pow   -> mul
         | term "/" pow   -> div
         | pow
-    ?expr  : expr "-" term  -> sub
-        | expr "+" term  -> add
-        | term
     ?pow   : atom "^" pow   -> exp
         | atom
     ?atom  : NUMBER                        -> number
@@ -36,26 +33,14 @@ grammar = Lark(
     """
 )
 
-exprs = [
-    "pi",
-    "sin"
-]
-
-
-for src in exprs:
-    tree = grammar.parse(src)
-    print(src)
-    print(tree.pretty())
-    print('-' * 40)
-
 class CalcTransformer(InlineTransformer):
-    from operator import add, sub, mul, truediv as div  # ... e mais! 
+    from operator import add, sub, mul, truediv as div, pow as exp, gt, ge, lt, le, ne, eq
 
     def __init__(self):
         super().__init__()
         self.variables = {k: v for k, v in vars(math).items() if not k.startswith("_")}
         self.variables.update(max=max, min=min, abs=abs)
-        self.vars = {}
+        self.tmp = {}
 
     def number(self, token):
         try:
@@ -63,25 +48,28 @@ class CalcTransformer(InlineTransformer):
         except:
             return float(token)
 
-    def assign(self, name, value):
-        self.env[name] = value
-        return self.env[name]
-
+    def const(self, token):
+        value = self.variables[token]
+        return value
 
     def var(self, token):
         if token in self.variables:
             return self.variables[token]
-        elif token[0].startswith('-') and token[1:] in self.variables:
+        elif token.startswith("-") and token[1:] in self.variables:
             return -self.variables[token[1:]]
         else:
-            return self.env[token]
-
+            return self.tmp[token]
+    
     def function_transform(self, name, *args):
         name = str(name)
         fn = self.variables[name.split('-')[-1]]
         if name.startswith('-'):
             return -fn(*args)
         return fn(*args)
-
+    
+    def assign(self, name, value):
+        self.tmp[name] = value
+        return self.tmp[name]
+    
     def start(self, *args):
         return args[-1]
